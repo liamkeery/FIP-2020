0aae1496ff5f1193e46c06dcdc9205e519c70f01
Syrrel Cabiling <syrrelcabiling@Syrrels-MacBook-Pro.local>
2020-02-02 20:47:40 -0500

    changed port; edited app.js and change res

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 const express = require('express');
 const path = require('path');
-
-
-// set the port
-const port = process.env.PORT || 3000;
-
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.set('views', path.join(__dirname, 'views'));
+// set the port
+const port = process.env.PORT || 3030;
 
 
-app.use('/', require('./routes/index'));
+
+// tell express where our static files are (js, images, css etc)
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/routes/index.html');
+});
 
 
-app.use((req, res, next) => {
-    var err = new Error('Not Found');
-    err.status = 404;
-    err.customMessage = "Hey! This page does not exist"
-    next(err); // means fall thorugh to the next route
-})
 
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ router.get('/', (req, res) => {
 
     // should really get the user data here and then fetch it thru, but let's try this asynchronously
     console.log('at the main route');
+
+    res.render('home');
     
 })
 
