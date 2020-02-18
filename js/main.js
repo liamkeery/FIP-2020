77741662e6b43bf94092c2fa23d0d681bd267d9e
syrrelCabiling <35806825+syrrelCabiling@users.noreply.github.com>
2020-02-18 17:33:26 -0500

    Splash page, typing effect, nav scroll in js, and scrollspy

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
 import Home from "./components/Home.js";
 
 
-// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
-Vue.component('', {
-  props: [''], //container where we can dump infos 
-  template: `
-    <div>  
-        <h3 class="movie-title">{{ movie.videotitle }}</h3>
-        <video :src="'video/' + movie.vidsource" controls autoplay></video>
-        <div class="movie-details">
-            <p>{{ movie.videodescription }}</p> 
-        </div>
-    </div>
-  `
-})
-
 const router = new VueRouter({
   routes: [
     { path: "/", 
@@ -40,3 +26,5 @@ var vm = new Vue({
   router
  
 }).$mount('#app');
+
+
