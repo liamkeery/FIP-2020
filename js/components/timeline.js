f24bb4a04514de999072b8ae3b7e704b221f6825
syrrelCabiling <35806825+syrrelCabiling@users.noreply.github.com>
2020-03-20 00:02:52 -0400

    timeline semi done

--- js/components/timeline.js
+++ js/components/timeline.js
@@ -12,9 +12,10 @@ export default {
             <div class="col-lg-7 mx-auto">
             <h1>Here’s some general education to get you started:</h1>
                 
-                <!-- Timeline -->
-                <ul class="timeline">
+            <!-- Timeline -->
+            <ul class="timeline">
             
+                <!-- FIRST ITEM -->
                     <li class="timeline-item bg-item-blue rounded ml-3 p-4 shadow">
                         <div class="timeline-arrow-blue"></div>
                         <img src="./images/guys.svg" class="mb-3 mr-3">
@@ -27,26 +28,70 @@ export default {
                         </p>
                     </li>
 
-
+                <!-- SECOND ITEM -->
                     <li class="timeline-item bg-gray rounded ml-3 p-4 shadow">
                         <div class="timeline-arrow-gray"></div>
                         <img src="./images/pill.png" class="mb-3 mr-3">
                         <h2 class="h5 mb-0 mt-2 float-left">1992</h2>
                         <p class="text-small mt-2 font-weight-light">First successful combination of drugs to treat AIDS are approved by the FDA.</p>
                     </li>
+
+                <!-- THIRD ITEM -->
                     <li class="timeline-item bg-item-blue rounded ml-3 p-4 shadow">
                         <div class="timeline-arrow-blue"></div>
-                        <img src="./images/pill.png" class="mb-3 mr-3">
-                        <h2 class="h5 mb-0">1998</h2>
-                        <p class="text-small mt-2 font-weight-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
+                        <img src="./images/pill.png" class="mb-3">
+                        <img src="./images/guys.svg" class="mb-3 mr-3">
+                        <h2 class="h5 mb-0 mt-2">1998</h2>
+                        <p class="text-small mt-2 font-weight-light">Post-Exposure Prohpylaxis (PEP) is introduced.</p>
+                        <ul class="mt-3">
+                            <li><b>PEP -  </b>Human Immunodeficiency Virus or HIV is a virus that attacks the body’s immune system. It weakens the body’s defenses, making it harder to fight off illness and infection. </li>
+                        </ul>
                     </li>
+
+                <!-- FOURTH ITEM -->
                     <li class="timeline-item bg-gray rounded ml-3 p-4 shadow">
                         <div class="timeline-arrow-gray"></div>
-                        <h2 class="h5 mb-0">Title of section 4</h2><span class="small text-gray"><i class="fa fa-clock-o mr-1"></i>10 October, 2019</span>
-                        <p class="text-small mt-2 font-weight-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
-                        <p class="text-small mt-2 font-weight-light">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+                        <img src="./images/hospital.png" class="mb-3 mr-3">
+                        <h2 class="h5 mb-0 mt-2">2012</h2>
+                        <p class="text-small mt-2 font-weight-light">Canada issues Pre-Exposure Prophylaxis (PrEP) and gains momentum. </p>
+                        <ul class="mt-3">
+                            <li><b>PrEP - </b>This is a prevention strategy used by an HIV-negative person who might be at higher risk of HIV infection. PrEP is taken before exposure to HIV. It blocks the virus from establishing itself in the body if the person taking it is exposed to the virus. </li>
+                        </ul>
                     </li>
-                </ul><!-- End -->
+
+                    <!-- FOURTH ITEM -->
+                    <li class="timeline-item bg-item-blue rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow-blue"></div>
+                        <img src="./images/canada.png" class="mb-3">
+                        <img src="./images/ok.png" class="mb-3 mr-3">
+                        <h2 class="h5 mb-0 mt-2">2016</h2>
+                        <p class="text-small mt-2 font-weight-light">Canada approves the use of PrEP.</p>
+                        </ul>
+                    </li>
+
+                    <!-- FOURTH ITEM -->
+                    <li class="timeline-item bg-item-blue rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow-blue"></div>
+                        <img src="./images/canada.png" class="mb-3">
+                        <img src="./images/ok.png" class="mb-3 mr-3">
+                        <h2 class="h5 mb-0 mt-2">2016</h2>
+                        <p class="text-small mt-2 font-weight-light">Canada approves the use of PrEP.</p>
+                        </ul>
+                    </li>
+
+                    <!-- FOURTH ITEM -->
+                    <li class="timeline-item bg-item-blue rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow-blue"></div>
+                        <img src="./images/canada.png" class="mb-3">
+                        <img src="./images/ok.png" class="mb-3 mr-3">
+                        <h2 class="h5 mb-0 mt-2">2016</h2>
+                        <p class="text-small mt-2 font-weight-light">Canada approves the use of PrEP.</p>
+                        </ul>
+                    </li>
+
+
+   
+            </ul><!-- End -->
 
             </div>
         </div>
@@ -59,3 +104,5 @@ export default {
         return{}
     }
 }
+
+
