b4d1afbed74b9bc63f8c5e52f71fad6a0099fdbf
syrrelCabiling <35806825+syrrelCabiling@users.noreply.github.com>
2020-04-12 15:46:55 -0400

    tweaks on responsiveness; added merch

--- js/components/merch.js
+++ js/components/merch.js
@@ -8,14 +8,13 @@ export default {
     <div class="row mx-auto text-center">
     
         <div class="col-lg-6 col-sm-12 mt-0 d-inline-block">
-            <img src="./images/merch1.png" alt="splash" class="merch1">
-            <img src="./images/merch1.png" alt="splash" class=" merch2">
-            <img src="./images/merch1.png" alt="splash" class="merch3">
-            <img src="./images/merch1.png" alt="splash" class="merch4">
-            <img src="./images/merch1.png" alt="splash" class=" merch5">
-            <img src="./images/merch1.png" alt="splash" class=" merch6">
-            <img src="./images/merch1.png" alt="splash" class="merch7">
-            <img src="./images/merch1.png" alt="splash" class="merch8">
+            <img src="./images/tounge.png" alt="splash" class="merch1">
+            <img src="./images/peach.png" alt="splash" class=" merch2">
+            <img src="./images/lips.png" alt="splash" class="merch3">
+            <img src="./images/lick.png" alt="splash" class="merch4">
+            <img src="./images/emojis.png" alt="splash" class=" merch5">
+            <img src="./images/eggplant.png" alt="splash" class=" merch6">
+            <img src="./images/cherries.png" alt="splash" class="merch7">
             <br>
         <a href="#"><button class="btn text-center mx-auto mt-3 mb-5 clearfix">BUY NOW</button></a>
 
